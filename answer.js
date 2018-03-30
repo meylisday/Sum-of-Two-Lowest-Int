059264d823243b00590bb70559374e001b64a93e
meylisday <victory.sett@gmail.com>
2018-03-30 11:13:59 +0300

    was added my solution

--- answer.js
+++ answer.js
@@ -1,19 +1,13 @@
-function sumTwoSmallestNumbers(numbers) {  
-  var frst = Math.min.apply(Math,numbers);
-  var othrs = [];
-  var dup = [];
-  for (s = 0; s < numbers.length; s++) {
-      if (numbers[s] !== frst) {
-          othrs.push(numbers[s]);
-      }
-      else if (numbers[s] === frst) {
-          dup.push(numbers[s]);
-      }
-  }
-  if (dup.length > 1) {
-      othrs.push(frst);
-  }
-  var scnd = Math.min.apply(Math,othrs);
-  var total = frst + scnd;
-  return total;
-}
+function sumTwoSmallestNumbers(numbers) {
+    a = 0;
+    b = 0;
+    sum = 0;
+    numbers.sort(function (a, b) { return a - b });
+    for (var i = 0; i < numbers.length; i++) {
+        a = numbers.slice(0, 1);
+        b = numbers.slice(1, 2);
+        sum = +a + +b;
+    }
+
+    return sum;
+};
